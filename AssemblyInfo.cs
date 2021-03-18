463838229596816d4a7299655405b3ae302a2999
Ksgz <rasibhelal@gmail.com>
2021-03-18 23:23:32 +0000

    Add project files.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EVASE Builder")]
+[assembly: AssemblyDescription("EVASE Screenshare Tool Builder")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EVASE")]
+[assembly: AssemblyProduct("EVASE Builder - https://evase.net")]
+[assembly: AssemblyCopyright("Copyright © EVASE 2014-2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("74b0f04f-f143-43a8-b7ea-107feb729775")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
